e2a69e95c579eaa92d20ecc0d0d6ab684c07deab
martinnmayer <martinnmayer@gmail.com>
2022-09-21 18:31:03 -0400

    unittests template file

---
+++ tests.py
@@ -0,0 +1,34 @@
+import unittest
+from source.qmr_exchange import Simulator, Exchange, Agent
+
+
+
+
+class TestAgent(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_agent(self):
+        pass
+
+    def test_naivemarketmaker(self):
+        pass
+
+    def test_randommarkettaker(self):
+        pass
+
+class TestSimulator(unittest.TestCase):
+    def setUp(self):
+        pass
+
+class TestExchange(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
