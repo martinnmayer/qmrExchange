8301c194465e0117cdd964f1bdcc2bcf77599f68
martinnmayer <martinnmayer@gmail.com>
2022-09-24 11:11:17 -0400

    docstring

--- source/helpers.py
+++ source/helpers.py
@@ -2,6 +2,11 @@ import plotly.graph_objects as go
 import pandas as pd
 
 def plot_bars(df):
+    """Returns a plotly OHLC chart.
+
+    Args:
+        df (dataframe): the dataframe containing the OHLCV data created by the simulation.
+    """
     fig = go.Figure(data=go.Ohlc(x=df.index,
                         open=df['open'],
                         high=df['high'],
