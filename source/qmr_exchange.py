77d43e8d492f1d352a2ebbcc1d8514687c8de6d4
martinnmayer <martinnmayer@gmail.com>
2022-10-04 09:34:44 -0400

    fix issue #3

--- source/qmr_exchange.py
+++ source/qmr_exchange.py
@@ -534,17 +534,19 @@ class Simulator():
 
     def get_portfolio_history(self,agent):
         agent = self.get_agent(agent)
-        portfolio = pd.DataFrame(index=self.datetime_range)
-        transactions = pd.DataFrame(agent._transactions).set_index('dt')
         bar_size =  get_pandas_time(self._time_unit)
+        portfolio = pd.DataFrame(index=get_datetime_range(self._from_date,self._to_date,self._time_unit))
+        transactions = pd.DataFrame(agent._transactions).set_index('dt')
         for ticker in list(self.exchange.books.keys()):
             qty_asset = pd.DataFrame(transactions[transactions['ticker']==ticker]['qty'])
             qty_asset = qty_asset.resample(bar_size).agg('sum').cumsum()
-            price = pd.DataFrame(self.get_price_bars(ticker=ticker,bar_size=bar_size)['close'].ffill())
-            portfolio[ticker] = (price['close'] * qty_asset['qty'])
-        portfolio['cash'] = transactions['cash_flow'].resample(bar_size).agg('sum').ffill().cumsum()
+            price = pd.DataFrame(index=get_datetime_range(self._from_date,self._to_date,self._time_unit))
+            price = price.join(self.get_price_bars(ticker=ticker,bar_size=bar_size)['close']).ffill()
+            price = price.join(qty_asset).ffill()
+            portfolio[ticker] = (price['close'] * price['qty'])
+        portfolio['cash'] = transactions['cash_flow'].resample(bar_size).agg('sum').ffill()
         portfolio.fillna(0,inplace=True)
-        portfolio['cash'] += agent.initial_cash
+        portfolio['cash'] = portfolio['cash'].cumsum()
         portfolio['aum'] = portfolio.sum(axis=1)
         return portfolio
 
